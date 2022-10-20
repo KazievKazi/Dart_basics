90632092fc82da777739e60e005af5c7d82b3bd5
Kazi <kazi.kaziev@list.ru>
2022-10-20 19:12:03 +0600

    task 3,4,5

--- main.dart
+++ main.dart
@@ -2,12 +2,17 @@ import 'dart:math' as Math;
 
 void main(List<String> args) {
   // DelimetersCalculator calculate = DelimetersCalculator();
-  WorkingWithConversion conversion = WorkingWithConversion();
+  // WorkingWithConversion conversion = WorkingWithConversion();
+  WorkingWithString checkString = WorkingWithString();
 
   // print(calculate.nod(72, 21));
   // print(calculate.nok([9, 12, 56, 89]));
   // print(calculate.numberByPrimeFactors(52, []));
-  print(conversion.decimalToBinary(810, []));
+  // print(conversion.decimalToBinary(810, []));
+  // print(checkString.stringToNum('one 2 three 4 five 6 seven 8 nine 10', []));
+  // print(checkString.collectionToMap( ['Red', 'Blue', 'Orange', 'Blue', 'Orange', 'Orange', 'Red', 'Blue'], {}));
+  print(checkString
+      .stringToList(['one', 'two', 'three', 'cat', 'dog', 'zero', 'zero'], []));
 }
 
 class DelimetersCalculator {
@@ -53,5 +58,58 @@ class WorkingWithConversion {
     }
     print(num);
     return num;
+  }
+}
+
+class WorkingWithString {
+  bool _isNumeric(String str) {
+    if (str == Null) {
+      return false;
+    }
+    return double.tryParse(str) != null;
+  }
+
+  List<dynamic> stringToNum(String text, List<int> arrayList) {
+    List array = text.split(' ');
+
+    for (int i = 0; i < array.length; i++) {
+      if (_isNumeric(array[i])) {
+        arrayList.add(int.parse(array[i]));
+      }
+    }
+    return arrayList;
+  }
+
+  Map<String, int> collectionToMap(List<String> array, Map<String, int> obj) {
+    for (int i = 0; i < array.length; i++) {
+      if (obj[array[i]] != null) {
+        obj[array[i]] = obj[array[i]]! + 1;
+      } else {
+        obj[array[i]] = 1;
+      }
+    }
+    return obj;
+  }
+
+  List<int> stringToList(List<String> arrText, List<int> array) {
+    Map<String, int> numberList = {
+      'zero': 0,
+      'one': 1,
+      'two': 2,
+      'three': 3,
+      'four': 4,
+      'five': 5,
+      'six': 6,
+      'sevet': 7,
+      'eght': 8,
+      'nine': 9
+    };
+
+    for (int i = 0; i < arrText.length; i++) {
+      if (numberList[arrText[i]] != null) {
+        array.add(numberList[arrText[i]]!.toInt());
+      }
+    }
+    return array.toSet().toList();
   }
 }
